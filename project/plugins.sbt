49474b2cbea4d19bbdb647721f2287e23180d15b
Kevin <lyterk@gmail.com>
2015-06-17 22:42:44 -0400

    First commit testing scala

---
+++ project/plugins.sbt
@@ -0,0 +1,3 @@
+
+addSbtPlugin("com.hanhuy.sbt" % "android-sdk-plugin" % "1.4.1")
+
---
+++ src/main/scala/com/lyterk/KevinActivity.scala
@@ -0,0 +1,12 @@
+package com.lyterk
+
+import android.app.Activity
+import android.os.Bundle
+
+class KevinActivity extends Activity {  
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)    
+  }
+}
